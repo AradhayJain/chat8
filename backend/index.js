5359759198fc8247baa96ff03decc0408dddeae6
AradhayJain <153743476+AradhayJain@users.noreply.github.com>
2024-12-26 21:27:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -87,9 +87,10 @@ app.use('/api/message',messageRoutes)
 
 const __dirname=path.resolve()
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')))
-    app.get("*",(req,res)=>res.sendFile 
-    (path.resolve(__dirname,"frontend","dist", "index.html")))
+    app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+    app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
 }
 else{
     app.get("/",(req,res)=>{
